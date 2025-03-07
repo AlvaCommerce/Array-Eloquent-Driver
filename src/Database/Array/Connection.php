5a9ae5853eef811133cacc99ac357ea28be33f5e
andrii <a.korposh@alvacommerce.com>
2025-03-07 15:03:14 +0200

    Fix support sql condition 'IN'

--- src/Database/Array/Connection.php
+++ src/Database/Array/Connection.php
@@ -19,7 +19,7 @@ class Connection extends ConnectionBase
                 return [];
             }
 
-            $params = array_combine($this->parseQuery($query), $this->prepareBindings($bindings));
+            $params = $this->parseQuery($query, $bindings);
 
             if (!isset($params['resolverClassName']) || !isset($params['resolverHandler'])) {
                 throw new RuntimeException('Invalid query');
@@ -33,15 +33,42 @@ class Connection extends ConnectionBase
 
             $rows = $resolverClass->{$resolverHandler}(...$params);
 
+            $select = $this->parseSelectQuery($query);
+
+
+            if (in_array('count(*) as aggregate', $select)) {
+                foreach ($rows as &$row) {
+                    $row['aggregate'] = count($rows);
+                }
+            }
+
             return $rows;
         });
     }
 
-    protected function parseQuery(string $query): array
+    protected function parseSelectQuery(string $query): array
     {
-        preg_match_all('/"([^"]+)"\s*=\s*\?/', $query, $matches);
-        $queryKeys = array_map(fn($key) => lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key)))), $matches[1]);
+        preg_match('/select\s+(.*?)\s+from/is', $query, $select);
+        return is_array($select[1]) ? $select[1] : explode(',', $select[1]);
+    }
 
-        return $queryKeys;
+    protected function parseQuery(string $query, array $bindings): array
+    {
+        preg_match_all('/["\']?([\w]+)["\']?\s+(=|IN)\s*(\(\s*\?(?:\s*,\s*\?)*\s*\)|\?)/i', $query, $matches);
+
+        $final = [];
+        $bindingIndex = 0;
+
+        foreach ($matches[1] as $colIndex => $column) {
+            $varName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $column))));
+            if (str_contains(strtolower($matches[2][$colIndex]), 'in')) {
+                $final[$varName] = array_slice($bindings, $bindingIndex, substr_count($matches[3][$bindingIndex], '?'));
+                $bindingIndex += count($final[$varName]);
+            } else {
+                $final[$varName] = $bindings[$bindingIndex];
+                $bindingIndex++;
+            }
+        }
+        return $final;
     }
 }
